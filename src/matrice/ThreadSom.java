d9fdfde7e6b4fef17653d0b7247f9696c85dea7e
OTHMANE8SIR <o.elmatlini2332@uca.ac.ma>
2026-03-31 11:39:14 +0100

    Matrice Multi-threads

---
+++ src/matrice/ThreadSom.java
@@ -0,0 +1,18 @@
+package matrice;
+
+public class ThreadSom extends Thread {
+
+    int[] t = null;
+    int som = 0;
+
+    public ThreadSom(int[] t) {
+        this.t = t;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < t.length; i++) {
+            som += t[i]; // Pour xhaque ligne on fournit un thread pour la somme
+        }
+    }
+}
